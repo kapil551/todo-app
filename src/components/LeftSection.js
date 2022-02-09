0d8becf35f46b1e993270474c40dd6c8ca106ef7
Kapil Chaudhary <chaudharikapil16@gmail.com>
2022-02-09 23:20:31 +0530

    added Input handler and button handler logic

--- src/components/LeftSection.js
+++ src/components/LeftSection.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LeftSection = () => {
 
+  let [todoTitle, setTodoTitle] = useState("");
+  let [todoDescription, setTodoDescription] = useState("");
+  let [todoData, setTodoData] = useState({});
+
+  const submitHandler = () => {
+    
+    setTodoData({
+      title: todoTitle,
+      description: todoDescription
+    });
+
+    console.log(todoData);
+
+    todoTitle = setTodoTitle("");
+    todoDescription = setTodoDescription("");
+  }
+
+  // console.log(todoTitle);
+  // console.log(todoDescription);
+
   return (
-
     <section className="bg-green-200 w-[50vw] pt-4">
-
       {/* title */}
       <div className="flex flex-col w-3/4 items-center justify-center space-y-4 bg-red-100 m-auto p-1">
         <div> TODO </div>
@@ -15,11 +33,26 @@ const LeftSection = () => {
           quisquam eligendi vero expedita? Debitis rem ut expedita, veritatis
           blanditiis laborum!
         </div>
-        <input type="text" placeholder="Title" />
-        <input type="text" placeholder="Description" />
-        <button className="bg-[#597EF7] w-44 text-white"> Add </button>
+        <input
+          type="text"
+          placeholder="Title"
+          onChange={(e) => {
+            setTodoTitle(e.target.value);
+          }}
+          value={todoTitle}
+          className="p-2"
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          onChange={(e) => {
+            setTodoDescription(e.target.value);
+          }}
+          value={todoDescription}
+          className="p-2"
+        />
+        <button className="bg-[#597EF7] w-44 text-white p-2" onClick={() => submitHandler()}> Add </button>
       </div>
-
     </section>
   );
 };
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,7 @@
 import React from "react";
-import { BsThreeDotsVertical } from 'react-icons/bs';
 import MenuDropDown from "./MenuDropDown";
 
 const TodoItem = () => {
-
-    const handleDropDown = () => {
-        console.log("Click");
-
-    }
 
     return (
         <div className="flex items-center justify-between border-b-2 border-gray-100">
@@ -16,10 +10,7 @@ const TodoItem = () => {
                 <div>Description comes here...</div>
             </div>
             <div className="cursor-pointer">
-                {/* <button className="" onClick={() => handleDropDown()}>
-                    <BsThreeDotsVertical />
-                </button> */}
-                <MenuDropDown className="bg-green-300" />
+                <MenuDropDown className="" />
             </div>
         </div>
     );
